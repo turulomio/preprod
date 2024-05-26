55c4dea92108b1be88536e363bd442700e3be651
turulomio <turulomio@yahoo.es>
2024-05-26 20:06:59 +0200

    preprod-0.2.0

--- preprod/__init__.py
+++ preprod/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="0.1.0"
-__versiondatetime__= datetime(2024, 4, 13, 8, 26)
+__version__="0.2.0"
+__versiondatetime__= datetime(2024, 5, 26, 20, 26)
 __versiondate__=__versiondatetime__.date()
